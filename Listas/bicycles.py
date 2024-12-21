531d1686332b597c4ca9ffecb33dda52e91692b2
Andreas Borba <borba_andreas@hotmail.com>
2024-12-21 03:41:32 -0300

    Inicio do estudo com listas

---
+++ Listas/bicycles.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+#Retorno o último elemento da lista, assim como -2 retorna o penultimo.
+print(bicycles[-1]) 

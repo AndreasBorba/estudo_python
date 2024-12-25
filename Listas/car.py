e04e870f52b9a209157fa58be41895b6e87189f5
Andreas Borba <borba_andreas@hotmail.com>
2024-12-25 02:33:57 -0300

    Atualização da utilização de listas

--- Listas/car.py
+++ Listas/car.py
@@ -6,3 +6,17 @@ print(cars)
 
 cars.sort(reverse=True)
 print(cars)
+
+car = ['bmw', 'audi', 'toyota', 'subaru']
+print("\nHere is the original list:")
+print(car)
+print("\nHere is the sorted list:")
+print(sorted(car)) #Pode ser usado o parâmetro reverse=True
+print("\nHere is the original list again:")
+print(car)
+
+car.reverse() #Tem o proposito de inverter a lista
+print(f"{car}")
+
+#Obtendo o tamanho de uma lista
+print(f"\n{len(car)}")
---
+++ Listas/practice_2.py
@@ -0,0 +1,14 @@
+places = ['Estados Unidos', 'Nova Zelandia', 'Australia', 'China', 'Japão']
+print(f"\n{places}")
+print(f"\n{sorted(places)}")
+print(f"\n{places}")
+print(f"\n{sorted(places, reverse=True)}")
+print(f"\n{places}")
+places.reverse();
+print(f"\n{places}")
+places.reverse();
+print(f"\n{places}")
+places.sort()
+print(f"\n{places}")
+places.sort(reverse=True)
+print(f"\n{places}")

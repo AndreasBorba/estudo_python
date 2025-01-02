0086fba9430bbb9bf36ae32d5a4f723a746952b6
Andreas Borba <borba_andreas@hotmail.com>
2025-01-02 10:58:12 -0300

    Trabalhando com iteração em python

---
+++ Iteracao/practice.py
@@ -0,0 +1,11 @@
+pizza = ['4queijo', 'frango e catupiry', 'calabresa']
+
+for nome_pizza in pizza:
+    print(f'Gosto de pizza de {nome_pizza.title()}.')
+print('Eu amo pizza!')
+
+animais = ['Cachorro', 'Gato', 'Papagaio']
+for animal in animais:
+    print(f'Um {animal} seria um ótimo animal de estimação.')
+print('Qualquer um desses animais seria um ótimo animal de estimação!')
+
---
+++ Iteracao/square_numbers.py
@@ -0,0 +1,15 @@
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print(squares)
+
+square = []
+for value in range(1, 11):
+    square.append(value ** 2)
+print(square)
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+min(digits)
+max(digits)
+sum(digits)
